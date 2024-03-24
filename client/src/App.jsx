912cad8923c35a707c163f1041647272c758e482
Marek Stefaniak <madgotten@wp.pl>
2024-03-24 21:11:27 +0100

    Add default refetchOnWindowFocus:false for whole app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React from 'react';
 import { AuthProvider } from './context/AuthContext';
 import { CartContextProvider } from './context/CartContext';
-import Footer from './components/Footer'
-import { BrowserRouter } from 'react-router-dom'
-import Paths from './Paths'
-import { QueryClient, QueryClientProvider } from 'react-query'
- 
-const queryClient = new QueryClient()
+import Footer from './components/Footer';
+import { BrowserRouter } from 'react-router-dom';
+import Paths from './Paths';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const App = () => {
   return (
     <div className='flex flex-col min-h-screen'>
       <BrowserRouter>
-      <div className="2xl:container mx-auto overflow-hidden w-full">
+        <div className='2xl:container mx-auto overflow-hidden w-full'>
           <AuthProvider>
             <CartContextProvider>
               <QueryClientProvider client={queryClient}>
@@ -20,11 +26,11 @@ const App = () => {
               </QueryClientProvider>
             </CartContextProvider>
           </AuthProvider>
-      </div>
-      <Footer/>
+        </div>
+        <Footer />
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

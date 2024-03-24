80615a514594f9ab2139497660780da0d6cc40de
Marek Stefaniak <madgotten@wp.pl>
2024-03-24 21:19:53 +0100

    Updated Navbar links names

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -3,15 +3,22 @@ import { categories } from '../assets/constants';
 
 const Categories = () => {
   return (
-    <div className="hidden md:flex md:flex-col md:flex-shrink-0 md:w-48 h-96">
-      <h2 className="text-blue-600 uppercase text-base font-semibold tracking-tight flex justify-center items-center p-2">Categories</h2>
-      <ul className="flex flex-col font-medium justify-between py-2 h-full">
-        {categories.map((category) => 
-          <a href={'/c/'+category.value} key={category.value}><li className="category-link">{category.title}<i className="fa-solid fa-chevron-right text-xs"></i></li></a>
-        )}
+    <div className='hidden md:flex md:flex-col md:flex-shrink-0 md:w-48 h-96'>
+      <h2 className='text-blue-600 uppercase text-base font-semibold tracking-tight flex justify-center items-center p-2'>
+        Categories
+      </h2>
+      <ul className='flex flex-col font-medium justify-between py-2 h-full'>
+        {categories.map((category) => (
+          <a href={'/c/' + category.value} key={category.value}>
+            <li className='category-link'>
+              {category.title}
+              <i className='fa-solid fa-chevron-right text-xs'></i>
+            </li>
+          </a>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Categories
+export default Categories;
--- server/routes/products.js
+++ server/routes/products.js
@@ -1,58 +1,57 @@
-const express = require('express')
-const Product = require('../models/product.model')
+const express = require('express');
+const Product = require('../models/product.model');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', async (req, res) => {
-    try {
-        const products = await Product.find({})
-        if (products && products.length > 0) {
-          res.status(200).json({ items: products })
-        } else {
-          res.sendStatus(404)
-        }
-      } catch (error) {
-        console.error(error)
-        res.sendStatus(500)
+  try {
+    const products = await Product.find({});
+    if (products && products.length > 0) {
+      res.status(200).json({ items: products });
+    } else {
+      res.sendStatus(404);
     }
-})
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
 
 router.get('/search', async (req, res) => {
-    if (!req.query.search) { 
-      return res.status(200).json({ items: []}) 
+  if (!req.query.search) {
+    return res.status(200).json({ items: [] });
+  }
+  const searchQuery = req.query.search.trim();
+  const searchContext = searchQuery.split(/\s+/);
+  const regex = searchContext.map((word) => new RegExp(word, 'i'));
+  const query = {
+    $and: regex.map((words) => ({ name: words })),
+  };
+  try {
+    const products = await Product.find(query).limit(5);
+    if (products && products.length > 0) {
+      res.status(200).json({ items: products });
+    } else {
+      res.status(200).json({ items: [] });
     }
-    const searchQuery = req.query.search.trim()
-    const searchContext = searchQuery.split(/\s+/)
-    const regex = searchContext.map(word => new RegExp(word, 'i'))
-    const query = {
-        $and: regex.map(words => ({ name: words }))
-    };
-    try {
-        const products = await Product.find(query).limit(5)
-        if (products && products.length > 0) {
-          res.status(200).json({ items: products })
-        } else {
-          res.status(200).json({ items: []})
-        }
-      } catch (error) {
-        console.error(error)
-        res.sendStatus(500)
-    }
-})
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
 
 router.get('/:id', async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id)
-        if (product) {
-          res.status(200).json({ items: product })
-        } else {
-          res.sendStatus(404)
-        }
-      } catch (error) {
-        console.error(error)
-        res.sendStatus(500)
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.status(200).json({ items: product });
+    } else {
+      res.sendStatus(404);
     }
-})
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
 
-
-module.exports = router
+module.exports = router;

ac68b7f8720076cb52cc8dbb38cd75b3a4a4216d
Marek Stefaniak <madgotten@wp.pl>
2024-08-10 15:03:37 +0200

    Change Dialog background to be darker and switch buttons order

--- client/src/context/DialogContext.jsx
+++ client/src/context/DialogContext.jsx
@@ -46,32 +46,35 @@ export const DialogProvider = ({ children }) => {
       {children}
       {isOpen &&
         createPortal(
-          <dialog
-            ref={dialogRef}
-            onCancel={handleCancel}
-            className="absolute left-0 top-0 bottom-0 max-w-96 bg-slate-50 shadow-lg drop-shadow-lg rounded-lg p-4 gap-2 flex flex-col items-center justify-between"
-          >
-            <div className="w-full flex justify-end text-lg">
-              <button className="hover:text-gray-500" onClick={handleCancel}>
-                &#10006;
-              </button>
-            </div>
-            <p className="mb-4 text-center">{message}</p>
-            <div className="flex gap-6">
-              <button
-                className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded text-sm"
-                onClick={() => handleConfirm()}
-              >
-                Confirm
-              </button>
-              <button
-                className="bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded text-sm"
-                onClick={handleCancel}
-              >
-                Cancel
-              </button>
-            </div>
-          </dialog>,
+          <>
+            <div className="fixed inset-0 bg-black bg-opacity-50 z-50"></div>
+            <dialog
+              ref={dialogRef}
+              onCancel={handleCancel}
+              className="absolute left-0 top-0 bottom-0 max-w-96 bg-slate-50 shadow-lg drop-shadow-lg rounded-lg p-4 gap-2 flex flex-col items-center justify-between"
+            >
+              <div className="w-full flex justify-end text-lg">
+                <button className="hover:text-gray-500" onClick={handleCancel}>
+                  &#10006;
+                </button>
+              </div>
+              <p className="mb-4 text-center">{message}</p>
+              <div className="flex gap-6">
+                <button
+                  className="bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded text-sm"
+                  onClick={handleCancel}
+                >
+                  Cancel
+                </button>
+                <button
+                  className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded text-sm"
+                  onClick={() => handleConfirm()}
+                >
+                  Confirm
+                </button>
+              </div>
+            </dialog>
+          </>,
           document.body
         )}
     </DialogContext.Provider>
